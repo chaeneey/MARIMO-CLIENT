675e615bf806117f6b1dd0e2291f2b45a21e7445
Parkchaeyeon <ccyy1029@naver.com>
2025-03-31 00:58:26 +0900

    chore: next 프로젝트 생성 및 폴더구조 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MARIMO",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
