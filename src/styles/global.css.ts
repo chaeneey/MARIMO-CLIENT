e2dadec3886f3bf32b6ef518fba264e224b216af
Parkchaeyeon <ccyy1029@naver.com>
2025-03-31 19:20:40 +0900

    chore: font 다운 및 세팅

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,4 +1,4 @@
-import { globalStyle } from "@vanilla-extract/css";
+import { globalFontFace, globalStyle } from "@vanilla-extract/css";
 
 import { base } from "./layers.css";
 import "./reset.css";
@@ -8,7 +8,7 @@ globalStyle(":root", {
   "@layer": {
     [base]: {
       vars: {
-        // "--min-width": "375px",
+        "--min-width": "375px",
         "--max-width": "700px",
       },
     },
@@ -24,9 +24,6 @@ globalStyle("html, body", {
       margin: 0,
       padding: 0,
       width: "100%",
-      //   display: "flex",
-      //   justifyContent: "center",
-      //   alignItems: "center",
       scrollBehavior: "smooth",
       overscrollBehavior: "none",
       WebkitTapHighlightColor: "transparent",
@@ -36,7 +33,15 @@ globalStyle("html, body", {
 
 // 폰트 설정
 globalStyle("body", {
-  fontFamily: `'Pretendard', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif`,
+  fontFamily: `'Pretendard', 'SUIT', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif`,
+});
+
+globalFontFace("Pretendard", {
+  src: 'url("/fonts/PretendardVariable.woff2") format("woff2-variations")',
+});
+
+globalFontFace("SUIT", {
+  src: 'url("/fonts/SUIT-Variable.woff2") format("woff2-variations")',
 });
 
 // A 태그 스타일
@@ -73,7 +78,7 @@ globalStyle("#root", {
   "@layer": {
     [base]: {
       width: "100%",
-      //   minWidth: "var(--min-width)",
+      minWidth: "var(--min-width)",
       maxWidth: "var(--max-width)",
       minHeight: "100dvh",
       backgroundColor: "#fff",
--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -34,30 +34,35 @@ export const vars = createGlobalTheme(":root", {
   fonts: {
     // 한글 head
     head01_32: {
+      fontFamily: "Pretendard",
       fontWeight: "700",
       fontSize: "3.2rem",
       lineHeight: "42px",
       letterSpacing: "-0.64px",
     },
     head02_28: {
+      fontFamily: "Pretendard",
       fontWeight: "700",
       fontSize: "2.8rem",
       lineHeight: "36px",
       letterSpacing: "-0.56px",
     },
     head03_28_m: {
+      fontFamily: "Pretendard",
       fontWeight: "500",
       fontSize: "2.8rem",
       lineHeight: "36px",
       letterSpacing: "-0.56px",
     },
     head04_22: {
+      fontFamily: "Pretendard",
       fontWeight: "700",
       fontSize: "2.2rem",
       lineHeight: "30px",
       letterSpacing: "-0.44px",
     },
     head05_20: {
+      fontFamily: "Pretendard",
       fontWeight: "700",
       fontSize: "2rem",
       lineHeight: "28px",
@@ -66,66 +71,77 @@ export const vars = createGlobalTheme(":root", {
 
     // 한글 body
     body01_24: {
+      fontFamily: "Pretendard",
       fontWeight: "500",
       fontSize: "2.4rem",
       lineHeight: "30px",
       letterSpacing: "-0.48px",
     },
     body02_22: {
+      fontFamily: "Pretendard",
       fontWeight: "600",
       fontSize: "2.2rem",
       lineHeight: "40px",
       letterSpacing: "-0.66px",
     },
     body03_20_36: {
+      fontFamily: "Pretendard",
       fontWeight: "600",
       fontSize: "2rem",
       lineHeight: "36px",
       letterSpacing: "-1px",
     },
     body04_20_34: {
+      fontFamily: "Pretendard",
       fontWeight: "600",
       fontSize: "2rem",
       lineHeight: "34px",
       letterSpacing: "-1px",
     },
     body05_20_24: {
+      fontFamily: "Pretendard",
       fontWeight: "600",
       fontSize: "2rem",
       lineHeight: "24px",
       letterSpacing: "-0.4px",
     },
     body06_20_m_24: {
+      fontFamily: "Pretendard",
       fontWeight: "500",
       fontSize: "2rem",
       lineHeight: "24px",
       letterSpacing: "0px",
     },
     body07_20_m_36: {
+      fontFamily: "Pretendard",
       fontWeight: "500",
       fontSize: "2rem",
       lineHeight: "36px",
       letterSpacing: "-1px",
     },
     body08_18_sb: {
+      fontFamily: "Pretendard",
       fontWeight: "600",
       fontSize: "1.8rem",
       lineHeight: "20px",
       letterSpacing: "0px",
     },
     body09_18_sb_narrow: {
+      fontFamily: "Pretendard",
       fontWeight: "600",
       fontSize: "1.8rem",
       lineHeight: "20px",
       letterSpacing: "-0.36px",
     },
     body10_18_b: {
+      fontFamily: "Pretendard",
       fontWeight: "700",
       fontSize: "1.8rem",
       lineHeight: "20px",
       letterSpacing: "0px",
     },
     body11_18_m: {
+      fontFamily: "Pretendard",
       fontWeight: "500",
       fontSize: "1.8rem",
       lineHeight: "20px",
@@ -134,36 +150,42 @@ export const vars = createGlobalTheme(":root", {
 
     // 영어
     eh0_head00_32_eb: {
+      fontFamily: "SUIT",
       fontWeight: "800",
       fontSize: "3.2rem",
       lineHeight: "40px",
       letterSpacing: "-0.64px",
     },
     eh1_head01_32: {
+      fontFamily: "SUIT",
       fontWeight: "700",
       fontSize: "3.2rem",
       lineHeight: "40px",
       letterSpacing: "-0.64px",
     },
     eh2_head02_28_b: {
+      fontFamily: "SUIT",
       fontWeight: "700",
       fontSize: "2.8rem",
       lineHeight: "35px",
       letterSpacing: "-0.56px",
     },
     eh3_head03_24_eb: {
+      fontFamily: "SUIT",
       fontWeight: "800",
       fontSize: "2.4rem",
       lineHeight: "30px",
       letterSpacing: "-0.48px",
     },
     eh4_head04_24_b: {
+      fontFamily: "SUIT",
       fontWeight: "700",
       fontSize: "2.4rem",
       lineHeight: "30px",
       letterSpacing: "-0.48px",
     },
     eh5_head05_18: {
+      fontFamily: "SUIT",
       fontWeight: "700",
       fontSize: "1.8rem",
       lineHeight: "22px",
