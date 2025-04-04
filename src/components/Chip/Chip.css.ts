1af6a2ca2c3b25822d1f2e1634fd7c30ffd9ffae
imhwarang <imhwarang4@naver.com>
2025-04-04 22:36:57 +0900

    feat: Chip 컴포넌트 구현 및 theme, tsconfig 수정

---
+++ src/components/Chip/Chip.css.ts
@@ -0,0 +1,15 @@
+import { flexGenerator } from "@styles/generator.css";
+import { vars } from "@styles/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const chipStyle = style([
+  vars.fonts.c3_caption_03_14,
+  flexGenerator(),
+  {
+    color: vars.colors.lime04,
+    backgroundColor: vars.colors.black,
+    padding: "0.2rem 0.9rem",
+    width: "fit-content",
+    borderRadius: "2px",
+  },
+]);
