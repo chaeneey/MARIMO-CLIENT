2a136a0d137fda0838b118e9f104425b981aadb2
imhwarang <imhwarang4@naver.com>
2025-04-05 01:10:10 +0900

    fix: lint 에러 수정

--- src/components/CustomImage/CustomImage.tsx
+++ src/components/CustomImage/CustomImage.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import Image from "next/image";
+import React from "react";
+
 import { imageStyle, imageWrapperStyle } from "./CustomImage.css";
 
 export interface ImageProps {
