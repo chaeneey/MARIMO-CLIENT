361c3dc593a0c14014a35520e082caf0d4db6e00
imhwarang <imhwarang4@naver.com>
2025-04-17 16:34:32 +0900

    SelectBox 스타일 수정

--- src/components/SelectBox/SelectBox.css.ts
+++ src/components/SelectBox/SelectBox.css.ts
@@ -4,12 +4,12 @@ import { flexGenerator } from "@styles/generator.css";
 import { vars } from "@styles/theme.css";
 
 export const selectBoxStyle = style([
-  vars.fonts.body08_18_sb,
+  vars.fonts.caption_03_14,
   flexGenerator("row", "space-between"),
   {
     width: "100%",
-    height: "6rem",
-    padding: "1.8rem 2.2rem",
+    height: "4.8rem",
+    padding: "1.4rem 2rem 1.4rem 1.8rem",
 
     color: vars.colors.gray05,
     backgroundColor: vars.colors.white,
@@ -48,7 +48,7 @@ export const ulStyle = style([
 ]);
 
 export const liStyle = style([
-  vars.fonts.body08_18_sb,
+  vars.fonts.caption_03_14,
   flexGenerator("column"),
   {
     width: "100%",
@@ -64,8 +64,8 @@ export const liOptionStyle = style([
   flexGenerator("row", "space-between"),
   {
     width: "100%",
-    height: "6rem",
-    padding: "1.9rem 2.2rem",
+    height: "4.8rem",
+    padding: "1.4rem 2rem 1.4rem 1.8rem",
 
     backgroundColor: vars.colors.white,
   },
@@ -74,22 +74,20 @@ export const liOptionStyle = style([
 export const liOptionLeftStyle = style([
   flexGenerator(),
   {
-    gap: "0.8rem",
+    gap: "0.163rem",
   },
 ]);
 
 export const liSubValueStyle = style([
-  vars.fonts.c3_caption_03_14,
+  vars.fonts.caption_03_14,
   {
     color: vars.colors.gray04,
   },
 ]);
 
 export const dividerStyle = style({
-  width: "100%",
+  width: "95%",
   height: "0.1rem",
-
-  margin: "0 1rem",
 
   backgroundColor: vars.colors.gray02,
 });
