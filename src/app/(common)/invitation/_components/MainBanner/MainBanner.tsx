04b2c732fdf718be0402973f53c549be86350ce1
imhwarang <imhwarang4@naver.com>
2025-05-01 21:48:18 +0900

    feat: MainBanner 구현

---
+++ src/app/(common)/invitation/_components/MainBanner/MainBanner.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { CustomImage } from "@/components";
+
+import {
+  bannerSectionStyle,
+  entireBar,
+  imageStyle,
+  presentBar,
+  progressBarContainer,
+} from "./MainBanner.css";
+
+const realImages = [
+  "/images/감딸기.jpeg",
+  "/images/상어.png",
+  "/images/짤2.jpeg",
+];
+const images = [
+  realImages[realImages.length - 1],
+  ...realImages,
+  realImages[0],
+];
+
+const MainBanner = () => {
+  const [currentIdx, setCurrentIdx] = useState(1);
+  const [transitioning, setTransitioning] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIdx((prev) => prev + 1);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [currentIdx]);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    if (currentIdx === 0) {
+      timeoutId = setTimeout(() => {
+        setTransitioning(false);
+        requestAnimationFrame(() => {
+          setCurrentIdx(images.length - 2);
+          requestAnimationFrame(() => {
+            setTransitioning(true);
+          });
+        });
+      }, 600);
+    } else if (currentIdx === images.length - 1) {
+      timeoutId = setTimeout(() => {
+        setTransitioning(false);
+        requestAnimationFrame(() => {
+          setCurrentIdx(1);
+          requestAnimationFrame(() => {
+            setTransitioning(true);
+          });
+        });
+      }, 600);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [currentIdx, images.length]);
+
+  const realIdx =
+    currentIdx === 0
+      ? realImages.length - 1
+      : currentIdx === images.length - 1
+        ? 0
+        : currentIdx - 1;
+
+  return (
+    <section className={bannerSectionStyle}>
+      <div
+        style={{
+          display: "flex",
+          width: `${images.length * 100}%`,
+          height: "100%",
+          transform: `translateX(-${currentIdx * (100 / images.length)}%)`,
+          transition: transitioning ? "transform 0.6s ease-in-out" : "none",
+        }}
+      >
+        {images.map((src, i) => (
+          <div key={i} className={imageStyle}>
+            <CustomImage src={src} alt={`banner-${i}`} />
+          </div>
+        ))}
+      </div>
+      <div className={progressBarContainer}>
+        <div className={entireBar}>
+          <div
+            className={presentBar}
+            style={{
+              transform: `translateX(${realIdx * 100}%)`,
+            }}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainBanner;
---
+++ src/app/(common)/invitation/page.tsx
@@ -0,0 +1,11 @@
+import { MainBanner } from "./_components";
+
+const page = () => {
+  return (
+    <div>
+      <MainBanner />
+    </div>
+  );
+};
+
+export default page;
