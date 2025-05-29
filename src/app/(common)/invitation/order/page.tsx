2ee49c25635c57ad120a4ece669a09585eae2f88
Parkchaeyeon <ccyy1029@naver.com>
2025-05-29 16:32:16 +0900

    feat: 반복되는 아코디언 map으로 변경하기

--- src/app/(common)/invitation/order/page.tsx
+++ src/app/(common)/invitation/order/page.tsx
@@ -16,6 +16,27 @@ import {
 } from "./_components";
 import * as styles from "./page.css";
 
+const accordionItems = [
+  { title: "주문자 정보", content: <OrderInfoSection /> },
+  { title: "청첩장 공통정보 입력", content: <CommonInfoSection /> },
+  { title: "종이 청첩장 기본 정보", content: <CoverInfoSection /> },
+  { title: "전세버스", hasCheckbox: true, content: <BusSection /> },
+  { title: "피로연", hasCheckbox: true, content: <PartySection /> },
+  {
+    title: "모바일 청첩장 기본 정보",
+    content: <CoverInfoSection type="mobile" />,
+  },
+  { title: "갤러리", hasCheckbox: true, content: <ImageDragBox /> },
+  { title: "전화걸기", hasCheckbox: true, content: <PhoneInfoSection /> },
+  { title: "축의금 계좌", hasCheckbox: true, content: <AccountSection /> },
+  { title: "달력", hasCheckbox: true },
+  { title: "지도 및 길 찾기", hasCheckbox: true },
+  { title: "방명록", hasCheckbox: true, content: <GuestSection /> },
+  { title: "카카오톡 공유", hasCheckbox: true },
+  { title: "참석의사 전달", hasCheckbox: true, content: <AttendSection /> },
+  { title: "기타 요청사항", hasCheckbox: true, content: <MemoSection /> },
+];
+
 const Page = () => {
   return (
     <form className={styles.orderPageLayout}>
@@ -31,59 +52,11 @@ const Page = () => {
       </div>
 
       <div className={styles.accordionContainer}>
-        <Accordion title="주문자 정보">
-          <OrderInfoSection />
-        </Accordion>
-
-        <Accordion title="청첩장 공통정보 입력">
-          <CommonInfoSection />
-        </Accordion>
-
-        <Accordion title="종이 청첩장 기본 정보">
-          <CoverInfoSection />
-        </Accordion>
-
-        <Accordion title="전세버스" hasCheckbox>
-          <BusSection />
-        </Accordion>
-
-        <Accordion title="피로연" hasCheckbox>
-          <PartySection />
-        </Accordion>
-
-        <Accordion title="모바일 청첩장 기본 정보">
-          <CoverInfoSection type="mobile" />
-        </Accordion>
-
-        <Accordion title="갤러리" hasCheckbox>
-          <ImageDragBox />
-        </Accordion>
-
-        <Accordion title="전화걸기" hasCheckbox>
-          <PhoneInfoSection />
-        </Accordion>
-
-        <Accordion title="축의금 계좌" hasCheckbox>
-          <AccountSection />
-        </Accordion>
-
-        <Accordion title="달력" hasCheckbox />
-
-        <Accordion title="지도 및 길 찾기" hasCheckbox />
-
-        <Accordion title="방명록" hasCheckbox>
-          <GuestSection />
-        </Accordion>
-
-        <Accordion title="카카오톡 공유" hasCheckbox />
-
-        <Accordion title="참석의사 전달" hasCheckbox>
-          <AttendSection />
-        </Accordion>
-
-        <Accordion title="기타 요청사항" hasCheckbox>
-          <MemoSection />
-        </Accordion>
+        {accordionItems.map(({ title, hasCheckbox, content }, idx) => (
+          <Accordion key={idx} title={title} hasCheckbox={hasCheckbox}>
+            {content && content}
+          </Accordion>
+        ))}
       </div>
     </form>
   );
