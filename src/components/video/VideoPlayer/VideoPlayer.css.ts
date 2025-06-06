fb8d5a399c9cd85d77f51ba25b5ee828e9149a23
Parkchaeyeon <ccyy1029@naver.com>
2025-06-07 00:39:30 +0900

    feat: 비디오 예시 추가하고, video 태그 이용

--- src/components/video/VideoPlayer/VideoPlayer.css.ts
+++ src/components/video/VideoPlayer/VideoPlayer.css.ts
@@ -3,10 +3,17 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "@styles/theme.css";
 
 export const videoPlayerLayout = style({
-  width: "100%",
-  height: "100dvh",
+  width: "100vw",
+  height: "100vh",
+  overflow: "hidden",
   backgroundColor: vars.colors.black,
   position: "relative",
+});
+
+export const videoStyle = style({
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
 });
 
 export const videoPlayerSoundIcon = style({
--- src/components/video/VideoPlayer/VideoPlayer.tsx
+++ src/components/video/VideoPlayer/VideoPlayer.tsx
@@ -4,17 +4,28 @@ import { useState } from "react";
 
 import { IcTurnOffSound, IcTurnOnSound } from "@/assets/svgs";
 
-import { videoPlayerLayout, videoPlayerSoundIcon } from "./VideoPlayer.css";
+import {
+  videoPlayerLayout,
+  videoPlayerSoundIcon,
+  videoStyle,
+} from "./VideoPlayer.css";
 
 const VideoPlayer = () => {
-  const [hasSound, setHasSound] = useState(false);
+  const [mutedState, setMutedState] = useState(true);
   const handleClickSoundIcon = () => {
-    setHasSound((prev) => !prev);
+    setMutedState((prev) => !prev);
   };
   return (
     <section className={videoPlayerLayout}>
+      <video
+        className={videoStyle}
+        src={"/video/pre-video.MP4"}
+        autoPlay
+        muted={mutedState}
+        loop
+      />
       <div className={videoPlayerSoundIcon} onClick={handleClickSoundIcon}>
-        {hasSound ? <IcTurnOnSound /> : <IcTurnOffSound />}
+        {mutedState ? <IcTurnOffSound /> : <IcTurnOnSound />}
       </div>
     </section>
   );
